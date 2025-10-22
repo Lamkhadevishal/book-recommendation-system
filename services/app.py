59dd7da58cfa68139188b7d00479391065a78ecc
vishal lamkhade <vishallamkhade21@gmail.com>
2025-10-23 00:14:45 +0530

    improvements and updations

--- services/app.py
+++ services/app.py
@@ -1,6 +1,9 @@
 from flask import Flask,render_template
 import pickle
 popular_df= pickle.load(open('popular.pkl','rb'))
+pt=pickle.load(open("pt.pkl",'rb'))
+book=pickle.load(open('book.pkl','rb'))
+similarity=pickle.load(open('similarity.pkl','rb'))
 
 
 
@@ -15,6 +18,14 @@ def index():
                            Votes=list(popular_df['num_of_rating'].values),
                            rating=list(popular_df['avg_of_rating'].values)
     )
+@app.route("/recommend")
+def recommend_ui():
+    return render_template("recommend.html")
+@app.route('/recommend_books')
+def recommend():
+   return 
+    
+
 
 if __name__=='__main__':
     app.run(debug=True)
